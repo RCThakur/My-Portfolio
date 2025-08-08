5afcffb546517dd17071d927b8af5188ecd6438e
RCThakur <rinkeshthakur1997@gmail.com>
2025-08-08 09:39:25 +0530

    Update home page with section alignments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { ThemeToggle } from "../components/ThemeToggle"
+// import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "../components/StarBackground"
 import { Navbar } from "../components/Navbar"
 import { HeroSection } from "../components/HeroSection"
@@ -12,7 +12,7 @@ export const Home = () => {
     return (
         <div className="min-h-screen bg-background text-foreground overflow-hidden">
             {/* Theme Toggle */}
-            <ThemeToggle />
+            {/* <ThemeToggle /> */}
 
             {/* Background Effect */}
             <StarBackground />
